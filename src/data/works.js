144a83d47e7e36b4e381eeb70fc064db7f7f7db8
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-19 16:26:03 +0530

    added works and nav link labels

---
+++ src/data/works.js
@@ -0,0 +1,60 @@
+
+
+export const works = [
+    "PHOTOGRAPHY", "VIDEO EDITOR", "UI & UX DESIGNER", "TUMBNAIL EDITOR", "Landscape Photography", "PRODUCT Photography", "Portrait  Photography"
+]
+
+export const footerNavLinks = [
+    {
+        label: "Home",
+        links: [
+            {
+                label: "About Me",
+                href: "#"
+            },
+            {
+                label: "My Works",
+                href: "#"
+            },
+            {
+                label: "Testimonials",
+                href: "#"
+            }
+
+        ]
+    },
+    {
+        label: "Portfolio",
+        links: [
+            {
+                label:"Product",
+                href: "#"
+            },
+            {
+                label:"Portrait",
+                href: "#"
+            },
+            {
+                label:"Landscape",
+                href: "#"
+            }
+        ]
+    },
+    {
+        label: "Services",
+        links: [
+            {
+                label: "Photography",
+                href: "#"
+            },
+            {
+                label: "Video Editing",
+                href: "#"
+            },
+            {
+                label: "UI & UX Design",
+                href: "#"
+            }
+        ]
+    }
+]

d9dc722c943baf62bd834da6140f68a06b67c39d
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-24 23:11:31 +0530

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});

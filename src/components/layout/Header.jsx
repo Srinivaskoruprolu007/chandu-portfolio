6adad53818c3a07669b3b2a68a1ceaaf0e7c5d8b
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-25 14:04:26 +0530

    added logo

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "/src/assets/react.svg";
+import logo from "/src/assets/logo.svg";
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -21,7 +21,10 @@ const Header = () => {
   // ✅ Close mobile menu when clicking outside
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (!event.target.closest(".mobile-menu") && !event.target.closest(".menu-button")) {
+      if (
+        !event.target.closest(".mobile-menu") &&
+        !event.target.closest(".menu-button")
+      ) {
         setShowMenu(false);
       }
     };
@@ -38,7 +41,9 @@ const Header = () => {
       {/* Logo and Mobile Menu Button */}
       <div className="flex justify-between w-full md:w-auto">
         <div className="logo mb-4 md:mb-0 ml-4 md:ml-0 md:mr-auto flex justify-start">
-          <img src={logo} alt="logo" className="h-8 w-8" />
+          <a href="/" className="hover:scale-105">
+            <img src={logo} alt="logo" className="h-16 w-16" />
+          </a>
         </div>
         <div className="md:hidden flex">
           <button className="outline-none menu-button" onClick={toggleMenu}>
@@ -64,14 +69,18 @@ const Header = () => {
             key={index}
             className={`p-2 text-white text-center px-8 hover:bg-gray-800 transition duration-300 
               ${index === 0 ? "first:rounded-l-lg" : ""} 
-              ${index === navItems.slice(0, 3).length - 1 ? "last:rounded-r-lg" : ""}`}
-            href="#" 
-          >                                              
+              ${
+                index === navItems.slice(0, 3).length - 1
+                  ? "last:rounded-r-lg"
+                  : ""
+              }`}
+            href="#"
+          >
             {item}
           </a>
-        ))}   
+        ))}
       </div>
-  
+
       {/* Contact Me Button (Desktop) */}
       <div className="hidden md:block p-2 rounded hover:bg-gray-800 hover:border hover:border-gray-500">
         <h2>{navItems[3]}</h2>
@@ -81,7 +90,11 @@ const Header = () => {
       {showMenu && (
         <div className="mobile-menu flex flex-col items-center space-y-4 bg-black w-full px-4 py-2 absolute left-0 top-full shadow-lg transition-all duration-300 z-50">
           {navItems.map((item, index) => (
-            <a key={index} className="border hover:bg-gray-800 p-2 rounded text-white text-center w-full" href="#">
+            <a
+              key={index}
+              className="border hover:bg-gray-800 p-2 rounded text-white text-center w-full"
+              href="#"
+            >
               {item}
             </a>
           ))}
@@ -92,5 +105,3 @@ const Header = () => {
 };
 
 export default Header;
-
- 

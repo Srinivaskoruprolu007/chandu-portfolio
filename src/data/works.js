d65645ca14e9c2b84e361ec0a51106b225a4ffd2
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-25 14:03:48 +0530

    added social links

--- src/data/works.js
+++ src/data/works.js
@@ -1,61 +1,72 @@
-
-
 export const works = [
-    "PHOTOGRAPHY", "VIDEO EDITOR", "UI & UX DESIGNER", "TUMBNAIL EDITOR", "Landscape Photography", "PRODUCT Photography", "Portrait  Photography"
-]
+  "PHOTOGRAPHY",
+  "VIDEO EDITOR",
+  "UI & UX DESIGNER",
+  "TUMBNAIL EDITOR",
+  "Landscape Photography",
+  "PRODUCT Photography",
+  "Portrait  Photography",
+];
 
 export const footerNavLinks = [
-    {
-        label: "Home",
-        links: [
-            {
-                label: "About Me",
-                href: "#"
-            },
-            {
-                label: "My Works",
-                href: "#"
-            },
-            {
-                label: "Testimonials",
-                href: "#"
-            }
-
-        ]
-    },
-    // removed this unwanted section in footerNavigation links
-    // {
-    //     label: "Portfolio",
-    //     links: [
-    //         {
-    //             label:"Product",
-    //             href: "#"
-    //         },
-    //         {
-    //             label:"Portrait",
-    //             href: "#"
-    //         },
-    //         {
-    //             label:"Landscape",
-    //             href: "#"
-    //         }
-    //     ]
-    // },
-    {
-        label: "Services",
-        links: [
-            {
-                label: "Photography",
-                href: "#"
-            },
-            {
-                label: "Video Editing",
-                href: "#"
-            },
-            {
-                label: "UI & UX Design",
-                href: "#"
-            }
-        ]
-    }
-]
+  {
+    label: "Home",
+    links: [
+      {
+        label: "About Me",
+        href: "#",
+      },
+      {
+        label: "My Works",
+        href: "#",
+      },
+      {
+        label: "Testimonials",
+        href: "#",
+      },
+    ],
+  },
+  // removed this unwanted section in footerNavigation links
+  // {
+  //     label: "Portfolio",
+  //     links: [
+  //         {
+  //             label:"Product",
+  //             href: "#"
+  //         },
+  //         {
+  //             label:"Portrait",
+  //             href: "#"
+  //         },
+  //         {
+  //             label:"Landscape",
+  //             href: "#"
+  //         }
+  //     ]
+  // },
+  {
+    label: "Services",
+    links: [
+      {
+        label: "Photography",
+        href: "#",
+      },
+      {
+        label: "Video Editing",
+        href: "#",
+      },
+      {
+        label: "UI & UX Design",
+        href: "#",
+      },
+    ],
+  },
+];
+// add social links
+export const socialLinks = {
+  linkedIn:
+    "https://www.linkedin.com/in/chandra-sekhar-kilaparthi-b894a024a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+  twitter: "https://x.com/chandu9k?s=21",
+  instagram:
+    "https://www.instagram.com/chandu.kilaparthi?igsh=MXRrNXNvemhxNGxuOQ%3D%3D&utm_source=qr",
+};

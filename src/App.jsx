a317367134f7588c3476d07bc8153edc3d8b8f5b
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-02-26 19:59:20 +0530

    added theme toggler and theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from "react";
-import Contactpage from "./pages/Contactpage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
 import Layout from "./components/layout/Index";
 import Hero from "./pages/Hero";
-
+import Contactpage from "./pages/Contactpage";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />} >
-          <Route index element={<Hero />} />
-           {/* <Route path="/about" element={<About />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Hero />} />
+            {/* <Route path="/about" element={<About />} />
           <Route path="/uiux" element={<Uiux />} />
           <Route path="/portfolio" element={<Portfolio />} /> */}
-          <Route path="/contact" element={<Contactpage />} />
+            <Route path="/contact" element={<Contactpage />} />
           </Route>
-         
         </Routes>
       </BrowserRouter>
-      
-    </>
+    </ThemeProvider>
   );
 }
 

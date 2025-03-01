53d01bb000ceb74b549714a8a24f5215a15c9891
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-03-01 13:56:26 +0530

    added imagekit configurations

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,12 +1,10 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import ImageKit from "imagekitio-react";
-
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export const imageKit = new ImageKit({
-  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
-  endpoint: process.env.IMAGE_KIT_ENDPOINT,
-});
+export const imageKit = {
+  publicKey: import.meta.env.VITE_IMAGE_KIT_PUBLIC_KEY,
+  endpoint: import.meta.env.VITE_IMAGE_KIT_ENDPOINT,
+};

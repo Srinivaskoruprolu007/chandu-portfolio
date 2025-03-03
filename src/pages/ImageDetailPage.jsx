9b2e308be7e84cc5b3e23e66c19bf7deada1b74f
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-03-03 18:34:11 +0530

    updated the code

--- src/pages/ImageDetailPage.jsx
+++ src/pages/ImageDetailPage.jsx
@@ -1,4 +1,5 @@
-import React, { useMemo } from "react";
+import React from "react";
+import { useMemo } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { IKImage } from "imagekitio-react";
